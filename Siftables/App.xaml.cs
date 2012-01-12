a520c10ba5c66a347930e7f80f51c1d7de0c2ca9
Alexander J Mullans <alex@alexmullans.com>
2012-01-11 19:45:10 -0500

    Updated basic UI design, edited program properties.

--- Siftables/App.xaml.cs
+++ Siftables/App.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace SLDemo
+namespace Siftables
 {
     public partial class App : Application
     {
@@ -26,7 +26,7 @@ namespace SLDemo
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            this.RootVisual = new MainPage();
+            this.RootVisual = new MainWindow();
         }
 
         private void Application_Exit(object sender, EventArgs e)
--- Siftables/Properties/AssemblyInfo.cs
+++ Siftables/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SLDemo")]
+[assembly: AssemblyTitle("Siftables Emulator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SLDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Singularity Software")]
+[assembly: AssemblyProduct("Siftables Emulator")]
+[assembly: AssemblyCopyright("© Siftables Emulator 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

a498f3c722bc2de8c4115ece6edf40e4fd33138b
Ethan Veatch <veatchje@rose-hulman.edu>
2012-01-19 16:14:30 -0500

    Added BaseApp

---
+++ Siftables/BaseApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Siftables
+{
+    public class BaseApp
+    {
+
+        public void setup()
+        {
+            // Override this method with information to initialize the application
+        }
+
+        public void tick()
+        {
+            // Override this method with periodic changes that are made when the application is running
+        }
+    }
+}

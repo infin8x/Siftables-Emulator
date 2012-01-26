f336592dd09cb4f21d111dee9e523d4c02fe2302
Alex Mullans <alex@alexmullans.com>
2012-01-26 00:31:30 -0500

    Finished diagram for M4.

---
+++ Siftables/CubeSet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Siftables
+{
+    public class CubeSet
+    {
+        List<Cube> cubes;
+    }
+}

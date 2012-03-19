4ae72bb8f5fafc6e10c26654a63898b1b2e165bc
Alexander J Mullans <alex@alexmullans.com>
2012-03-18 21:13:48 -0400

    Adding residual changes from Saturday night.

--- Siftables/Cube.xaml.cs
+++ Siftables/Cube.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 using System.Windows.Interactivity;
-using Siftables;
-using System.Windows.Media.Imaging;
 using Siftables.Behaviors;
 
 namespace Siftables
@@ -26,6 +14,5 @@ namespace Siftables
             Interaction.GetBehaviors(this).Add(new DragAndDropBehavior());
             Interaction.GetBehaviors(this).Add(new NeighborRefreshBehavior());
         }
-        
     }
 }

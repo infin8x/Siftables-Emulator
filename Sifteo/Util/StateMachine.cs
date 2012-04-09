16bcac18b09159abe8bb9a9d3419e35c98cec1d3
Alex Mullans <alex@alexmullans.com>
2012-04-09 09:16:52 -0400

    Create StateMachine test and class.
    Signed-off-by: Alex Mullans <alex@alexmullans.com>

---
+++ Sifteo/Util/StateMachine.cs
@@ -0,0 +1,13 @@
+﻿namespace Sifteo.Util
+{
+    public class StateMachine
+    {
+        
+        public void State(string state, StateFunction func)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public delegate string StateFunction(string transitionId);
+    }
+}
---
+++ Test/StateMachineTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sifteo.Util;
+
+namespace Test
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        private StateMachine _sm;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sm = new StateMachine();
+        }
+        [TestMethod]
+        public void TestNewStateMachine()
+        {
+            Assert.IsInstanceOfType(_sm, typeof(StateMachine));
+        }
+
+        [TestMethod]
+        public void TestState()
+        {
+            _sm.State("State1", state1Controller);
+        }
+
+        [TestMethod]
+        public void TestStateFunction()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestCurrentStateGet()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}

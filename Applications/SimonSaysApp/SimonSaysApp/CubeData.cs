f8ebd01a505e4a5366d98aad74fa542b83a4e20d
Richard Thai <richardpthai@gmail.com>
2012-05-11 08:22:58 -0400

    Started a Simon Says Game

---
+++ Applications/SimonSaysApp/SimonSaysApp/CubeData.cs
@@ -0,0 +1,24 @@
+﻿using Sifteo;
+
+namespace SimonSaysApp
+{
+    internal class CubeData
+    {
+        private int _color;
+
+        public CubeData(int color)
+        {
+            _color = color;
+        }
+
+        public int GetCubeColor()
+        {
+            return _color;
+        }
+
+        public void SetCubeColor(int color)
+        {
+            _color = color;
+        }
+    }
+}
